cc34d1a7fd8d518837e328fcfa3e4c36fa35874d
DrTexx <23412100+DrTexx@users.noreply.github.com>
2023-12-20 16:22:02 +1100

    recurse through dir contents + wip handler for links

--- print_tree_from_json.py
+++ print_tree_from_json.py
@@ -8,11 +8,13 @@ def handle_tree_item(tree_item):
             handle_tree_file(tree_item)
         elif tree_item_type == 'directory':
             handle_tree_directory(tree_item)
+        elif tree_item_type == 'link':
+            handle_tree_link(tree_item)
         else:
             raise Exception("it's not a file or directory! ah!")
     else:
         raise Exception("i dunno")
-        
+
 def handle_tree_file(tree_file):
     # TODO(Denver): actually implement this
     print("it's a file!")
@@ -20,6 +22,15 @@ def handle_tree_file(tree_file):
 def handle_tree_directory(tree_dir):
     # TODO(Denver): actually implement this
     print("it's a dir!")
+    print(f"name: {tree_dir['name']}")
+    tree_dir_contents = tree_dir['contents']
+    print(f"contents: {len(tree_dir_contents)}")
+    for tree_item in tree_dir_contents:
+        handle_tree_item(tree_item)
+
+def handle_tree_link(tree_link):
+    # TODO(Denver): actually implement this
+    print(tree_link)
 
 with open('./.cache/tree.json') as f:
     data = json.load(f)
